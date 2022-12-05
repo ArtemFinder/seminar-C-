895736e8400224a6c0be82927dcc391dd0580644
Artem <logik562@ya.ru>
2022-12-06 01:51:43 +0700

    первая задача

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Значение а= ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Значение b= ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine("МАХ = " + a);
+else
+    Console.WriteLine("МАХ = " + b);
