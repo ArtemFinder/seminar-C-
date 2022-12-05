b7caccf50a637946094e06d0ead3454304482e12
Artem <logik562@ya.ru>
2022-12-06 02:00:48 +0700

    вторая задача

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Значение а= ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Значение b= ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Значение c= ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+    Console.WriteLine("МАХ = " + a);
+else if (a < b && b > c)
+    Console.WriteLine("МАХ = " + b);
+else Console.WriteLine("МАХ = " + c);
