dd9421b381f02873fc9cf01f3da062b295c85ff6
Artem <logik562@ya.ru>
2022-12-06 02:22:16 +0700

    четвертая задача

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Список всех чётных чисел до числа N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i < n)
+{
+    i++;
+    if (i % 2 == 0)
+        Console.Write(i + ", ");
+}
