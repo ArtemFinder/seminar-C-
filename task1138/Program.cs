5b643692ce834b9380206a4ef0d106131874e3e4
Artem <logik562@ya.ru>
2022-12-07 18:39:03 +0700

    доп.задача 1138

---
+++ task1138/Program.cs
@@ -0,0 +1,17 @@
+﻿//доп задача второго семинара
+Console.Clear();
+int n = 0;
+int max = 0;
+int max2 = 0;
+Console.WriteLine("Введите последовательность чисел. 0 для завершения ввода последовательности.");
+do
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max)
+    {
+        max2 = max;
+        max = n;
+    }
+}
+while (n != 0);
+Console.WriteLine($"Второй максимум в этой последовательности: {max2}");
