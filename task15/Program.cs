c7e8c91325f82698a6e4ecb2f798cf484233293f
Artem <logik562@ya.ru>
2022-12-07 18:21:48 +0700

    задача 15

---
+++ task15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("введите номер дня недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && x < 6) Console.WriteLine("Пора вjobывать!");
+else if (x == 6 || x == 7) Console.WriteLine("ВЫХОДНОЙ!");
+else Console.WriteLine("Совсем заработался... Пора в отпуск!"); //На тот случай если забыл сколько дней в неделе :)
