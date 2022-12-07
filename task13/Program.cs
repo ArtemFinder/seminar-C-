3932954650bf0fbc6b774864557127a04c1dc57c
Artem <logik562@ya.ru>
2022-12-07 18:07:48 +0700

    задача 13

---
+++ task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int x3 = 0;
+if (x < 100) Console.Write("Третьей цифры нет.");
+else
+{
+    x3 = x;
+    while (x3 > 999) x3 = x3 / 10;
+}
+Console.WriteLine("Третья цифра: " + x3 % 10);
