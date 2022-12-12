145d20e70a4be030845b7511438896b07056ea54
Artem <logik562@ya.ru>
2022-12-13 02:00:41 +0700

    задача 19

---
+++ task19/Program.cs
@@ -0,0 +1,7 @@
+﻿// задача №19
+Console.Write("Введите пятизначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x / 1000 == x - (x / 100 * 100))
+    Console.Write($"{x} - это палиндром");
+else
+    Console.Write($"{x} - это не палиндром");
