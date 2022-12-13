7d5d0492a066f387a9642c694e4960933040945f
Artem <logik562@ya.ru>
2022-12-14 04:04:37 +0700

    исправление_ошибки

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,7 +1,10 @@
 ﻿// задача №19
 Console.Write("Введите пятизначное число: ");
 int x = Convert.ToInt32(Console.ReadLine());
-if (x / 1000 == x - (x / 100 * 100))
+int xl = x / 1000;
+int xr = (x % 100 - x % 10) / 10 + x % 10 * 10;
+Console.Write($"{xr} - это ");
+if (xl == xr)
     Console.Write($"{x} - это палиндром");
 else
     Console.Write($"{x} - это не палиндром");
