24cc8891b6f29760821b815a74ad6256ec3713bd
Artem <logik562@ya.ru>
2022-12-19 23:32:35 +0700

    Задачи 34, 36, 38

---
+++ task34/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Random rnd = new Random();
+Console.WriteLine("Введите длину массива: ");
+int[] arrgh = new int[Convert.ToInt32(Console.ReadLine())];
+int chetko = 0;
+for (int i = 0; i < arrgh.Length; i++)
+{
+    arrgh[i] = rnd.Next(100, 1000);
+    if (arrgh[i] % 2 == 0) chetko++;
+}
+Console.WriteLine($"В массиве [{string.Join(", ", arrgh)}] количество чётных чисел {chetko}");
---
+++ task38/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+Random rnd = new Random();
+double[] arrgh = new double[10];
+double max = 0;
+double min = 100;
+for (int i = 0; i < arrgh.Length; i++)
+{
+    arrgh[i] = rnd.Next(0, 100); //в задании вещественные числа, а в примере 
+    if (arrgh[i] >= max) max = arrgh[i];
+    if (arrgh[i] <= min) min = arrgh[i];
+}
+Console.WriteLine($"В массиве [{string.Join(", ", arrgh)}] разница между {max} и {min} = {max - min}");
