4f83b7729d9e91426ed110d0d59852450ff93c0d
Artem <logik562@ya.ru>
2022-12-23 13:29:56 +0700

    задачи 41,43

---
+++ task41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int s = 0;
+void summ_array(int[] array, out int d) //хотелось попробовать процедуру, хотя здесь в ней не вижу необходимости.
+{
+    d = 0;
+    for (int i = 0; i < array.Length; i++) if (array[i] > 0) d++;
+}
+
+Console.WriteLine("Введите несколько чисел через запятую: ");
+int[] a = Console.ReadLine().Split(", ").Select(x => int.Parse(x)).ToArray();
+summ_array(a, out s);
+Console.WriteLine($"Введено {s} числа больше 0.");
