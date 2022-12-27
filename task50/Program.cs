b089be9ea26261e668248cce77bcdf0df96092f8
Artem <logik562@ya.ru>
2022-12-27 14:28:36 +0700

    задачи 47, 50 ,52

---
+++ task50/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 50.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7->такой позиции в массиве нет (пользователь вводит нумерацию с 1)
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindXY(int[,] matrix, int x, int y)
+{
+    if (matrix.GetLength(0) < y || matrix.GetLength(1) < x)
+        Console.Write($"{x}, {y} - такой позиции в массиве нет");
+    else Console.Write($"{x}, {y} = {matrix[y - 1, x - 1]}");
+}
+
+Console.Clear();
+int[,] matrix = new int[4, 5];
+InputMatrix(matrix);
+
+Console.Write("Введите какой элемент вывести: ");
+int[] xy = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+FindXY(matrix, xy[0], xy[1]);
---
+++ task52/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 52.Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Average(int[,] matrix)
+{
+    double y = 0;
+    Console.WriteLine("Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        y = 0;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            y = y + matrix[j, i];
+            if (matrix.GetLength(0) == j + 1) Console.Write($"{y / matrix.GetLength(0)} \t");
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Average(matrix);
