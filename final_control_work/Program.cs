75ffb2447e8cca6efd6d457e59237602edafbb5d
Artem <logik562@ya.ru>
2023-01-27 00:45:17 +0700

    блок-схема

--- final_control_work/Program.cs
+++ final_control_work/Program.cs
@@ -4,4 +4,14 @@ void printArray(string[] array)
     for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} \t");
 }
 
-string[] array
+void inputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = Console.ReadLine();
+}
+
+Console.WriteLine($"Введите длину массива");
+string[] array = new string[Convert.ToInt32(Console.ReadLine())];
+Console.WriteLine($"Введите {array.Length} слов через Enter");
+inputArray(array);
+Console.WriteLine("Массив где слова не длинее 3 символов");
+printArray(array);
