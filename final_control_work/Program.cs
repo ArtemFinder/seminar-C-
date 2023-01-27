2ef2508ff6cc2a78bdbc15a3b476e4bcc5274830
Artem <logik562@ya.ru>
2023-01-27 21:16:17 +0700

    программа

--- final_control_work/Program.cs
+++ final_control_work/Program.cs
@@ -1,2 +1,38 @@
 ﻿//Итоговая проверочная работа.
+void printArray(string[] array) //Вывод массива
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} \t");
+}
+
+void inputArray(string[] array) //ввод значений массива
+{
+    for (int i = 0; i < array.Length; i++) array[i] = Console.ReadLine();
+}
+
+void copyArray(string[] array, out string[] array2) //копирование массива, но только значения 
+{
+    array2 = new string[array.Length];
+    int index_three = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[index_three] = array[i];
+            index_three++;
+        }
+    }
+}
+
 Console.Clear();
+Console.WriteLine($"Введите длину массива");
+string[] array = new string[Convert.ToInt32(Console.ReadLine())];
+Console.WriteLine($"Через Enter введите в количестве {array.Length} шт.");
+inputArray(array);
+
+Console.WriteLine("В массиве: ");
+printArray(array);
+Console.WriteLine();
+Console.WriteLine("значения не длинее 3 символов:");
+string[] array_three = new string[0];
+copyArray(array, out array_three);
+printArray(array_three);
